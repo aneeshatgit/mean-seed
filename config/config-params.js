71add1f25e69e4c094fb16fddce0cce72ff35279
Aneesh Raj <aneesh.raj@gmail.com>
2014-04-08 16:35:47 +0530

    added config-params

---
+++ config/config-params.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.session_secret = 'asdfsaASDJGA9ejhas93masdnnalZEDOJwiidnsp';
+config.mongo_dev_string = 'mongodb://localhost/sample';
+config.mongo_prod_string = 'mongodb://localhost/sample';
+
+
+module.exports = config;
